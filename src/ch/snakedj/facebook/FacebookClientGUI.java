d775ab2a69f801367fb960804ff3f809efe09ca7
Corsin Capol <corsin.capol@gmail.com>
2009-10-31 14:10:14 +0100

    correct gmail url

--- src/ch/snakedj/facebook/FacebookClientGUI.java
+++ src/ch/snakedj/facebook/FacebookClientGUI.java
@@ -30,74 +30,75 @@ import com.google.gdata.util.ContentType;
 import com.google.gdata.util.ServiceException;
 
 public class FacebookClientGUI {
-	
+
 	Text txtUsernameFacebook;
 	Text txtPasswordFacebook;
 	Text txtUsernameGmail;
 	Text txtPasswordGmail;
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		FacebookClientGUI fcg = new FacebookClientGUI();
 		fcg.create();
 	}
-	
-	public FacebookClientGUI()
-	{
+
+	public FacebookClientGUI() {
 
 	}
-	
-	public void create()
-	{
+
+	public void create() {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setSize(1024, 768);
-		
+
 		GridLayout layMain = new GridLayout();
 		layMain.numColumns = 2;
-		shell.setLayout(layMain);					
-		
+		shell.setLayout(layMain);
+
 		GridLayout layFacebookGmail = new GridLayout();
 		layFacebookGmail.numColumns = 2;
-		
-		//Facebook Group		
+
+		// Facebook Group
 		Group grpFacebook = new Group(shell, SWT.NONE);
 		grpFacebook.setText("Facebook");
 		grpFacebook.setLayout(layFacebookGmail);
-		
+
 		Label lblUsernameFacebook = new Label(grpFacebook, SWT.NONE);
 		lblUsernameFacebook.setText("Username: ");
 		txtUsernameFacebook = new Text(grpFacebook, SWT.BORDER);
 		Label lblPasswordFacebook = new Label(grpFacebook, SWT.NONE);
 		lblPasswordFacebook.setText("Password:");
-		txtPasswordFacebook = new Text(grpFacebook, SWT.BORDER | SWT.PASSWORD);		
-		//Facebook Group
-		
-		//Gmail Group		
+		txtPasswordFacebook = new Text(grpFacebook, SWT.BORDER | SWT.PASSWORD);
+		// Facebook Group
+
+		// Gmail Group
 		Group grpGmail = new Group(shell, SWT.NONE);
 		grpGmail.setText("GMail");
 		grpGmail.setLayout(layFacebookGmail);
-		
+
 		Label lblUsernameGmail = new Label(grpGmail, SWT.NONE);
 		lblUsernameGmail.setText("Username: ");
 		txtUsernameGmail = new Text(grpGmail, SWT.BORDER);
 		Label lblPasswordGmail = new Label(grpGmail, SWT.NONE);
 		lblPasswordGmail.setText("Password:");
-		txtPasswordGmail = new Text(grpGmail, SWT.BORDER | SWT.PASSWORD);		
-		//Gmail Group
-		
+		txtPasswordGmail = new Text(grpGmail, SWT.BORDER | SWT.PASSWORD);
+		// Gmail Group
+
 		Button btnNext = new Button(shell, SWT.NONE);
 		btnNext.setText("Synchronize");
+
 		btnNext.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
 				int countNotMatched = 0;
 				int countMatched = 0;
 				try {
-					ContactsService myService = new ContactsService("ch.snakedj.facebook");
+					ContactsService myService = new ContactsService(
+							"ch.snakedj.facebook");
 					GmailClient gmailClient = new GmailClient();
 					gmailClient.setUsername(txtUsernameGmail.getText());
 					try {
-						myService.setUserCredentials(txtUsernameGmail.getText(), txtPasswordGmail.getText());
+						myService.setUserCredentials(
+								txtUsernameGmail.getText(), txtPasswordGmail
+										.getText());
 						gmailClient.loadAllContacts(myService);
 					} catch (AuthenticationException e1) {
 						e1.printStackTrace();
@@ -106,11 +107,11 @@ public class FacebookClientGUI {
 					} catch (IOException e2) {
 						e2.printStackTrace();
 					}
-					
-					System.out.println("-----------------------------------------------------------");
-					
-					
-					FacebookClient facebookClient;								
+
+					System.out
+							.println("-----------------------------------------------------------");
+
+					FacebookClient facebookClient;
 					facebookClient = new FacebookClient();
 					facebookClient.setUsername(txtUsernameFacebook.getText());
 					facebookClient.setPassword(txtPasswordFacebook.getText());
@@ -124,45 +125,48 @@ public class FacebookClientGUI {
 						e1.printStackTrace();
 					}
 					List<User> friends = facebookClient.getFriends();
-					
-					for(User friend:friends)
-					{
-						ContactEntry gmailContact = gmailClient.getContact(friend.getLastName()+"_"+friend.getFirstName());
-						
-						if(gmailContact == null)
-						{
-							gmailContact = gmailClient.getContact(friend.getFirstName()+"_"+friend.getLastName());
-						}						
-						
-						if(gmailContact != null)
-						{
+
+					for (User friend : friends) {
+						ContactEntry gmailContact = gmailClient
+								.getContact(friend.getLastName() + "_"
+										+ friend.getFirstName());
+
+						if (gmailContact == null) {
+							gmailContact = gmailClient.getContact(friend
+									.getFirstName()
+									+ "_" + friend.getLastName());
+						}
+
+						if (gmailContact != null) {
 							countMatched++;
-							
-							if(gmailContact.getContactPhotoLink().getEtag() == null && friend.getPicBig() != null)
-							{
+
+							if (gmailContact.getContactPhotoLink().getEtag() == null
+									&& friend.getPicBig() != null) {
 								updateProfilePhoto(myService, friend,
 										gmailContact);
 
-							    System.out.print("PHOTO ");
+								System.out.print("PHOTO ");
 							}
-							
-//							Birthday birthday = gmailContact.getBirthday();
-//							System.out.println(birthday.getWhen());
-							
-							
-							System.out.println(friend.getLastName()+"_"+friend.getFirstName());
-						}
-						else
-						{
+
+							// Birthday birthday = gmailContact.getBirthday();
+							// System.out.println(birthday.getWhen());
+
+							System.out.println(friend.getLastName() + "_"
+									+ friend.getFirstName());
+						} else {
 							countNotMatched++;
-							System.err.println(friend.getLastName()+"_"+friend.getFirstName());
-						}											
+							System.err.println(friend.getLastName() + "_"
+									+ friend.getFirstName());
+						}
 					}
-					
+
 				} catch (FacebookException e1) {
 					e1.printStackTrace();
 				}
-				System.out.println("not matched" + countNotMatched + " matched " + countMatched);
+				
+				System.out.println("not matched" + countNotMatched
+						+ " matched " + countMatched);
+
 			}
 
 			private void updateProfilePhoto(ContactsService myService,
@@ -171,26 +175,27 @@ public class FacebookClientGUI {
 				URL photoUrl;
 				try {
 					photoUrl = new URL(photoLink.getHref());
-					GDataRequest request = myService.createRequest(GDataRequest.RequestType.UPDATE,
-						    photoUrl, new ContentType("image/jpeg"));
+					GDataRequest request = myService.createRequest(
+							GDataRequest.RequestType.UPDATE, photoUrl,
+							new ContentType("image/jpeg"));
 
-						    request.setEtag(photoLink.getEtag());
+					request.setEtag(photoLink.getEtag());
 
-						    OutputStream requestStream = request.getRequestStream();
-						    
-						    URL urlPic = new URL(friend.getPicBig());
+					OutputStream requestStream = request.getRequestStream();
 
-							BufferedInputStream in = new BufferedInputStream(urlPic
-									.openStream());
-							
-							byte data[] = new byte[1];
-							while (in.read(data, 0, 1) >= 0) {
-								requestStream.write(data);
-							}																					    
-							
-						    request.execute();
-						    in.close();
-				
+					URL urlPic = new URL(friend.getPicBig());
+
+					BufferedInputStream in = new BufferedInputStream(urlPic
+							.openStream());
+
+					byte data[] = new byte[1];
+					while (in.read(data, 0, 1) >= 0) {
+						requestStream.write(data);
+					}
+
+					request.execute();
+					in.close();
+
 				} catch (MalformedURLException e1) {
 					e1.printStackTrace();
 				} catch (IOException e5) {
@@ -201,16 +206,13 @@ public class FacebookClientGUI {
 			}
 		});
 
-		
-		shell.open();		
-		while(!shell.isDisposed())
-		{
-			if(!display.readAndDispatch())
-			{
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 		display.close();
 	}
-	
+
 }
